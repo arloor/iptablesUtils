f309af4093feba318f0d02416b7c1c691739eecf
arloor <admin@arloor.com>
2019-04-13 02:02:42 +0800

    修改

--- iptables4ddns.sh
+++ iptables4ddns.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# wget  https://raw.githubusercontent.com/arloor/iptablesUtils/master/iptables4ddns.sh;bash iptables4ddns.sh;rm -f iptables4ddns.sh;
 
 
 remotehost=example.com #中转目标host，自行修改
@@ -73,7 +74,7 @@ echo  重新设置iptables转发
 rm -f deletePre.sh
 wget https://raw.githubusercontent.com/arloor/iptablesUtils/master/deletePre.sh
 chmod +x deletePre.sh
-iptables -L PREROUTING -n -t nat |grep DNAT|grep dpt:8888|awk '{print $8}'|tail -1|tr "\n" " "|xargs -d :  ./deletePre.sh
+iptables -L PREROUTING -n -t nat |grep DNAT|grep dpt:$localport|awk '{print $8}'|tail -1|tr "\n" " "|xargs -d :  ./deletePre.sh
 rm -f deletePre.sh
 iptables -L PREROUTING -n -t nat --line-number|grep dpt:$localport|awk  '$1!=""{print $1}'|sort -r|xargs -n 1  iptables -t nat  -D PREROUTING
 

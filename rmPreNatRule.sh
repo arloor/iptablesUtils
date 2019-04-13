9b7931762c7ff7b162a44d5244cb96fc74671399
arloor <admin@arloor.com>
2019-04-13 12:03:42 +0800

    修改

--- rmPreNatRule.sh
+++ rmPreNatRule.sh
@@ -9,7 +9,7 @@ if [ "$1" == "" ];then
 fi
 
 if [ "$3" = "" ];then
-    iptables -L PREROUTING -n -t nat |grep DNAT|grep dpt:$1|awk '{print $8}'|tail -1|tr "\n" " "|xargs -d :  ./deletePre.sh
+    iptables -L PREROUTING -n -t nat |grep DNAT|grep dpt:$1|awk '{print $8}'|tail -1|tr "\n" " "|xargs -d :  ./rmPreNatRule.sh
     iptables -L PREROUTING -n -t nat --line-number|grep dpt:$1|awk  '$1!=""{print $1}'|sort -r|xargs -n 1  iptables -t nat  -D PREROUTING
 else
     iptables -L POSTROUTING -n -t nat --line-number|grep $2|grep $3|awk  '{print $1}'|sort -r|xargs -n 1 iptables -t nat  -D POSTROUTING
